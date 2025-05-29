bce8dc4a6015559aa3805583d58074bdb3c66728
nickx00 <75399141+n1ckisthereu@users.noreply.github.com>
2025-05-29 20:32:25 -0300

    feat: update lazyvim and remove old configurations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,32 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
-if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono Nerd Font:h10"
-  vim.api.nvim_set_keymap(
-    "n",
-    "<C-+>",
-    ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>",
-    { silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    "n",
-    "<C-->",
-    ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>",
-    { silent = true }
-  )
-  vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
 
-  vim.g.neovide_transparency = 1
-  vim.g.transparency = 1
-  --
-  -- vim.api.nvim_set_keymap("n", "<C-+>",
-  --   ":lua vim.g.neovide_transparency = math.min(vim.g.neovide_transparency + 0.05, 1.0)<CR>", { silent = true })
-  -- vim.api.nvim_set_keymap("n", "<C-_>",
-  --   ":lua vim.g.neovide_transparency = math.max(vim.g.neovide_transparency - 0.05, 0.0)<CR>", { silent = true })
-  --
-  -- vim.api.nvim_set_keymap("n", "<C-S-I)>", ":lua vim.g.neovide_transparency = 0.9<CR>", { silent = true })
-end
+require("markdown-utils")
+require("markdown-toc")
 
 vim.opt.swapfile = false
+vim.g.snacks_animate = false
+
+vim.g.moonflyTransparent = true
+vim.g.moonflyCursorColor = true
+vim.g.moonflyUnderlineMatchParen = true
